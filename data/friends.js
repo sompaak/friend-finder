c268fdc55300bb37e74c211195446139ce7bcb93
Akhil <akhil.sompalli@snhu.edu>
2018-03-05 17:47:42 -0800

    added better image url's

--- data/friends.js
+++ data/friends.js
@@ -2,33 +2,33 @@
 
 	[
 		  {
-		    name: "Ahmed",
-		    photo: "ahmed@example.com",
+		    name: "Tyrion Lannister",
+		    photo: "http://images.cdn.starpulse.com/news/bloggers/1279398/blog_images/tyrion-1.jpg",
 		    "score": [2,5,3,2,1,3,4,3,2,1]
 		    
 		  },
 		  {
-		    name: "james",
-		    photo: "ahmed@example.com",
+		    name: "Daenarys",
+		    photo: "https://www.telegraph.co.uk/content/dam/fashion/2017/06/19/TELEMMGLPICT000131421802_trans_NvBQzQNjv4BqkUE_BTgBOQu3VWKvpDGX9fr7sARQy7EgBjwPUzvqL_M.jpeg?imwidth=450",
 		    "score": [4,3,2,1,5,4,3,2,3,4]
 
 
 		  },
 		  {
-		    name: "bob",
-		    photo: "https://www.patiliyo.com/wp-content/uploads/2017/07/ruyada-kedi-gormek.jpg",
+		    name: "Jon snow",
+		    photo: "https://nerdist.com/wp-content/uploads/2017/08/Game-of-Thrones-Political-Ads.jpg",
 		    "score": [3,2,5,2,3,1,2,1,2,1]
 		    
 		  },
 		  {
-		    name: "frank",
-		    photo: "ahmed@example.com",
+		    name: "Margaery Tyrell",
+		    photo: "https://vignette.wikia.nocookie.net/gameofthrones/images/1/17/Margaery_Tyrell_Valar_Dress.png/revision/latest?cb=20120626001421",
 		    "score": [1,3,5,3,2,4,2,1,3,1]
 		    
 		  },
 		  {
-		    name: "Bill",
-		    photo: "ahmed@example.com",
+		    name: "Jaime Lannister",
+		    photo: "https://i.pinimg.com/originals/88/23/f8/8823f8358c164546a2b7450e431533b0.gif",
 		    "score": [5,3,2,1,4,5,3,2,2,1]   
 		  }
 
